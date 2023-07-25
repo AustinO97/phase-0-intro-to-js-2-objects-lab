f8ecb9f8f9fc83780e8b97e54416af4419ac4de2
Austin Oblow <austinoblow97@gmail.com>
2023-07-25 13:49:58 -0500

    completed lab

--- index.js
+++ index.js
@@ -2,54 +2,25 @@ const employee = {
     name: 'Sam',
     streetAdress: '11 Broadway',
 };
-/*
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-  employee[key] = value
 
-  return employee;
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
 };
-
-const newEmployee = updateEmployeeWithKeyAndValue (
-    employee,
-    'Jim',
-    '1234',
-);
-*/
-newEmployee;
-
-function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newObj = { ...employee};
-
-    newObj[key] = value;
-
-    return newObj;
-}
-
-const newnew = updateEmployeeWithKeyAndValue (
-    employee,
-    'Jim',
-    '1234'
-);
-newObj;
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
-
     return employee;
-}
-const newOne = destructivelyUpdateEmployeeWithKeyAndValue (employee, "yo", " 4321");
-newOne;
+};
 
 function deleteFromEmployeeByKey(employee, key) {
-    const newObj = { ...employee};
-
-    delete newObj[key]
-
-    return newObj;
-}
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key]
-    return employee
-}
-
+    delete employee.name;
+    return employee;
+};
